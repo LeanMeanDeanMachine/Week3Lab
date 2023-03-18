0806b3f69f3babf033fb9ac300a8599a29dd9d1f
LeanMeanDeanMachine <126035068+LeanMeanDeanMachine@users.noreply.github.com>
2023-03-18 12:24:46 -0600

    int array with supplied values

--- src/LabWeek3.java
+++ src/LabWeek3.java
@@ -1,5 +1,8 @@
 public class LabWeek3 {
     public static void main(String[] args) {
         //create an array with the following values: 1, 5, 2, 8, 13, 6
+        int[] array = {1, 5, 2, 8, 13, 6};
+
+
     }
 }
